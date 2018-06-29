c484ff9e1ae385648269ec8e22858961bedab0fe
abbyssoul <abbyssoul@gmail.com>
2018-06-29 13:23:10 +1000

    9pdecoder example handles requests and responses

--- examples/9pdecode.cpp
+++ examples/9pdecode.cpp
@@ -17,7 +17,7 @@
 #include "styxe/9p2000.hpp"
 #include "styxe/print.hpp"
 
-#include <solace/base64.hpp>
+#include <solace/base16.hpp>
 
 #include <iostream>
 #include <fstream>
@@ -31,8 +31,10 @@ using namespace styxe;
 std::ostream& operator<< (std::ostream& ostr, ImmutableMemoryView const& view) {
     if (view.size() > 0) {
         // We use custom output printing each byte as \0 bytest and \n are not printable otherwise.
-        for (const auto& b : view) {
-            ostr << std::hex << static_cast<int>(b);
+        auto i = base16Encode_begin(view);
+        auto end = base16Encode_end(view);
+        for (;i != end; ++i) {
+            ostr << *i;
         }
     } else {
         ostr << "<null>";
@@ -56,32 +58,31 @@ std::ostream& operator<< (std::ostream& ostr, Protocol::OpenMode mode) {
     return ostr;
 }
 
+std::ostream& operator<< (std::ostream& ostr, Protocol::Qid const& qid) {
+    return ostr << "{"
+                << "type: " << static_cast<int>(qid.type) << ", "
+                << "ver: "  << qid.version << ", "
+                << "path: " << qid.path
+                << "}";
 
-std::ostream& operator<< (std::ostream& ostr, Protocol::Stat const& stat) {
-    ostr << "{"
-         << "size: " << stat.size << ", "
-         << "type: " << stat.type << ", "
-         << "dev: " << stat.dev << ", "
-         << "qid: {"
-             << "type: " << stat.qid.type << ", "
-             << "ver: " << stat.qid.version << ", "
-             << "path: " << stat.qid.path << ", "
-         << "}, "
-         << "mode: " << stat.mode << ", "
-         << "atime: " << stat.atime << ", "
-         << "mtime: " << stat.mtime << ", "
-         << "length: " << stat.length << ", "
-
-         << "name: \"" << stat.name << "\", "
-         << "uid: \"" << stat.uid << "\", "
-         << "gid: \"" << stat.gid << "\", "
-         << "muid: \"" << stat.muid << "\""
-         << "}";
-
-    return ostr;
 }
 
-
+std::ostream& operator<< (std::ostream& ostr, Protocol::Stat const& stat) {
+    return ostr << "{"
+         << "size: "    << stat.size    << ", "
+         << "type: "    << stat.type    << ", "
+         << "dev: "     << stat.dev     << ", "
+         << "qid: "     << stat.qid     << ", "
+         << "mode: "    << stat.mode    << ", "
+         << "atime: "   << stat.atime   << ", "
+         << "mtime: "   << stat.mtime   << ", "
+         << "length: "  << stat.length  << ", "
+         << "name: \""  << stat.name    << "\", "
+         << "uid: \""   << stat.uid     << "\", "
+         << "gid: \""   << stat.gid     << "\", "
+         << "muid: \""  << stat.muid    << "\""
+         << "}";
+}
 
 
 void onVersionRequest(Protocol::Request::Version& req) {
@@ -137,7 +138,8 @@ void onWStatRequest(Protocol::Request::WStat& req) {
 
 
 void onWalkRequest(Protocol::Request::Walk& req) {
-    std::cout << req.fid << " " << req.newfid << " "
+    std::cout << req.fid << " "
+              << req.newfid << " "
               << req.path.getComponentsCount() << " [";
 
     for (auto& c: req.path) {
@@ -148,7 +150,7 @@ void onWalkRequest(Protocol::Request::Walk& req) {
 
 
 void onSessionRequest(Protocol::Request::Session& req) {
-    std::cout << req.key << std::endl;
+    std::cout << wrapMemory(req.key) << std::endl;
 }
 
 
@@ -164,45 +166,140 @@ void onShortWriteRequest(Protocol::Request::SWrite& req) {
 }
 
 
+void onVoidResponse() {
+    std::cout << std::endl;
+}
+
+void onReadResponse(Protocol::Response::Read const& resp) {
+    std::cout << resp.data.size()
+              << " DATA[" << resp.data << "]"
+              << std::endl;
+}
+
+void onWriteResponse(Protocol::Response::Write const& resp) {
+    std::cout << resp.count
+              << std::endl;
+}
+
+void onStatResponse(Protocol::Stat const& stat) {
+    std::cout << stat << std::endl;
+}
+
+void onCreateResponse(Protocol::Response::Create const& resp) {
+    std::cout << resp.qid << " " << resp.iounit << std::endl;
+}
+
+void onOpenResponse(Protocol::Response::Open const& resp) {
+    std::cout << resp.qid << " " << resp.iounit << std::endl;
+}
+
+void onAttachResponse(Protocol::Response::Attach const& resp) {
+    std::cout << resp.qid << std::endl;
+}
+
+void onAuthResponse(Protocol::Response::Auth const& resp) {
+    std::cout << resp.qid << std::endl;
+}
+
+void onVersionResponse(Protocol::Response::Version const& resp) {
+    std::cout << resp.msize << " \"" << resp.version << "\"" <<std::endl;
+}
+
+void onErrorResponse(Protocol::Response::Error const& resp) {
+    std::cout << " \"" << resp.ename << "\"" <<std::endl;
+}
+
+void onWalkResponse(Protocol::Response::Walk const& resp) {
+    std::cout << resp.nqids
+              << "[";
+     for (uint i = 0; i < resp.nqids; ++i) {
+         std::cout << resp.qids[i] << " ";
+     }
+     std::cout << "]" << std::endl;
+}
+
+
+
+void dispayRequest(Protocol::Request&& req) {
+    switch (req.type()) {
+    case Protocol::MessageType::TVersion:     onVersionRequest(req.asVersion());     break;
+    case Protocol::MessageType::TAuth:        onAuthRequest(req.asAuth());           break;
+    case Protocol::MessageType::TAttach:      onAttachRequest(req.asAttach());       break;
+    case Protocol::MessageType::TFlush:       onFlushRequest(req.asFlush());         break;
+    case Protocol::MessageType::TWalk:        onWalkRequest(req.asWalk());           break;
+    case Protocol::MessageType::TOpen:        onOpenRequest(req.asOpen());           break;
+    case Protocol::MessageType::TCreate:      onCreateRequest(req.asCreate());   break;
+    case Protocol::MessageType::TRead:        onReadRequest(req.asRead());           break;
+    case Protocol::MessageType::TWrite:       onWriteRequest(req.asWrite());      break;
+    case Protocol::MessageType::TClunk:       onClankRequest(req.asClunk());         break;
+    case Protocol::MessageType::TRemove:      onRemoveRequest(req.asRemove());    break;
+    case Protocol::MessageType::TStat:        onStatRequest(req.asStat());           break;
+    case Protocol::MessageType::TWStat:       onWStatRequest(req.asWstat());      break;
+
+    case Protocol::MessageType::TSession:     onSessionRequest(req.asSession());  break;
+    case Protocol::MessageType::TSRead:       onShortReadRequest(req.asShortRead());    break;
+    case Protocol::MessageType::TSWrite:      onShortWriteRequest(req.asShortWrite());break;
+
+    default:
+        std::cerr << "<Unknown message type>" << std::endl;
+    }
+}
+
+void dispayResponse(Protocol::Response&& resp) {
+    switch (resp.type) {
+    case Protocol::MessageType::RVersion:     onVersionResponse(resp.version);     break;
+    case Protocol::MessageType::RAuth:        onAuthResponse(resp.auth);           break;
+    case Protocol::MessageType::RAttach:      onAttachResponse(resp.attach);       break;
+    case Protocol::MessageType::RError:       onErrorResponse(resp.error);      break;
+    case Protocol::MessageType::RFlush:       onVoidResponse();         break;
+    case Protocol::MessageType::RWalk:        onWalkResponse(resp.walk);           break;
+    case Protocol::MessageType::ROpen:        onOpenResponse(resp.open);           break;
+    case Protocol::MessageType::RCreate:      onCreateResponse(resp.create);   break;
+    case Protocol::MessageType::RRead:        onReadResponse(resp.read);           break;
+    case Protocol::MessageType::RWrite:       onWriteResponse(resp.write);      break;
+    case Protocol::MessageType::RClunk:       onVoidResponse();         break;
+    case Protocol::MessageType::RRemove:      onVoidResponse();    break;
+    case Protocol::MessageType::RStat:        onStatResponse(resp.stat);           break;
+    case Protocol::MessageType::RWStat:       onVoidResponse();      break;
+
+    case Protocol::MessageType::RSession:     onVoidResponse();  break;
+    case Protocol::MessageType::RSRead:       onReadResponse(resp.read);    break;
+    case Protocol::MessageType::RSWrite:      onWriteResponse(resp.write);  break;
+
+    default:
+        std::cerr << "<Unknown message type>" << std::endl;
+    }
+}
+
+
 void readAndPrintMessage(std::istream& in, ByteBuffer& buffer, styxe::Protocol& proc) {
-    in.read(buffer.viewRemaining().dataAs<char>(), buffer.limit());
-    const size_t got = in.gcount();
-    buffer.limit(got);
+
+    // Message header is fixed size - so it is safe to attempt to read it.
+    in.read(buffer.viewRemaining().dataAs<char>(), proc.headerSize());
+    buffer.limit(in.gcount());
 
     proc.parseMessageHeader(buffer)
             .then([&](Protocol::MessageHeader&& header) {
-//                std::cout << "→ [" << std::setw(5) << header.size << "] " << header.type << " " << header.tag << ": ";
-                std::cout << "→ [" << std::setw(5) << header.size << "] <" << header.tag << ">" << header.type << ": ";
+                buffer.clear();
+                in.read(buffer.viewRemaining().dataAs<char>(), header.size - proc.headerSize());
+                buffer.limit(in.gcount());
 
-                return proc.parseRequest(header, buffer);
+                bool const isRequest = (static_cast<byte>(header.type) % 2) == 0;
+                if (isRequest) {
+                    std::cout << "→ [" << std::setw(5) << header.size << "] <" << header.tag << ">" << header.type << ": ";
+
+                    proc.parseRequest(header, buffer)
+                            .then(dispayRequest);
+                } else {
+                    std::cout << "→ [" << std::setw(5) << header.size << "] " << header.type << " " << header.tag << ": ";
+
+                    proc.parseResponse(header, buffer)
+                            .then(dispayResponse);
+                }
             })
-    .then([&](Protocol::Request&& req) {
-        switch (req.type()) {
-        case Protocol::MessageType::TVersion:     onVersionRequest(req.asVersion());     break;
-        case Protocol::MessageType::TAuth:        onAuthRequest(req.asAuth());           break;
-        case Protocol::MessageType::TAttach:      onAttachRequest(req.asAttach());       break;
-        case Protocol::MessageType::TFlush:       onFlushRequest(req.asFlush());         break;
-        case Protocol::MessageType::TWalk:        onWalkRequest(req.asWalk());           break;
-        case Protocol::MessageType::TOpen:        onOpenRequest(req.asOpen());           break;
-        case Protocol::MessageType::TCreate:      onCreateRequest(req.asCreate());   break;
-        case Protocol::MessageType::TRead:        onReadRequest(req.asRead());           break;
-        case Protocol::MessageType::TWrite:       onWriteRequest(req.asWrite());      break;
-        case Protocol::MessageType::TClunk:       onClankRequest(req.asClunk());         break;
-        case Protocol::MessageType::TRemove:      onRemoveRequest(req.asRemove());    break;
-        case Protocol::MessageType::TStat:        onStatRequest(req.asStat());           break;
-        case Protocol::MessageType::TWStat:       onWStatRequest(req.asWstat());      break;
-
-        case Protocol::MessageType::TSession:     onSessionRequest(req.asSession());  break;
-        case Protocol::MessageType::TSRead:       onShortReadRequest(req.asShortRead());    break;
-        case Protocol::MessageType::TSWrite:      onShortWriteRequest(req.asShortWrite());break;
-
-        default:
-            std::cerr << "<Unknown message type>" << std::endl;
-        }
-    })
-    .orElse([](Error&& err) {
-        std::cerr << "Error parsing message: " << err.toString() << std::endl;
-    });
+            .orElse([](Error&& err) {
+                std::cerr << "Error parsing message header: " << err.toString() << std::endl;
+            });
 }
 
 
@@ -210,9 +307,14 @@ void readAndPrintMessage(std::istream& in, ByteBuffer& buffer, styxe::Protocol& 
  * A simple example of decoding a 9P message from a file / stdin and printing it in a human readable format.
  */
 int main(int argc, const char **argv) {
-    styxe::Protocol proc;
+
+    styxe::Protocol::size_type maxMessageSize = styxe::Protocol::MAX_MESSAGE_SIZE;
+    // TODO: Parse cmd line options for usage, msize, procVersion
+
+    styxe::Protocol proc(maxMessageSize);
     MemoryManager memManager(proc.maxPossibleMessageSize());
     ByteBuffer buffer(memManager.create(proc.maxPossibleMessageSize()));
+
 
     if (argc > 1) {
         for (int i = 1; i < argc; ++i) {
